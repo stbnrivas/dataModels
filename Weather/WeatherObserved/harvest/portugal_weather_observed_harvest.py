1bdaa82571136327c479b057c6c3f042b0fe0535
Jose M. Cantera <jmcanterafonseca@gmail.com>
2018-03-26 11:02:15 +0200

    Portugal command line parameters - Only latest observations (#233)
    * Harvester parameters. Only retrieve latest when requested
    
    * Fixing latest adjustments
    
    * Portugal changes-Need to test
    
    * Only harvesting specific stations if needed
    
    * Final tweak

--- Weather/WeatherObserved/harvest/portugal_weather_observed_harvest.py
+++ Weather/WeatherObserved/harvest/portugal_weather_observed_harvest.py
@@ -1,6 +1,7 @@
 #!bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
 import urllib2
 import StringIO
 import csv
@@ -27,8 +28,12 @@ persisted_stations = 0
 total_stations = 0
 
 MIME_JSON = 'application/json'
-FIWARE_SERVICE = 'Weather'
-FIWARE_SPATH = '/Portugal'
+fiware_service = None
+fiware_service_path = None
+
+only_latest = False
+
+stations_to_retrieve_data = []
 
 
 def decode_wind_direction(direction):
@@ -62,6 +67,9 @@ def get_weather_observed_portugal():
 
         for date in source_data:
             for station_code in source_data[date]:
+                if len(stations_to_retrieve_data) > 0 and station_code not in stations_to_retrieve_data:
+                    continue
+
                 if station_code not in observation_data:
                     observation_data[station_code] = []
 
@@ -142,18 +150,27 @@ def post_station_data_batch(station_code, data):
     if len(data) == 0:
         return
 
+    data_to_be_stored = data
+
+    if only_latest:
+        data_to_be_stored = [data[-1]]
+
     data_obj = {
         'actionType': 'APPEND',
-        'entities': data
+        'entities': data_to_be_stored
     }
     data_as_str = json.dumps(data_obj)
 
     headers = {
         'Content-Type': MIME_JSON,
-        'Content-Length': len(data_as_str),
-        'Fiware-Service': FIWARE_SERVICE,
-        'Fiware-Servicepath': FIWARE_SPATH
+        'Content-Length': len(data_as_str)
     }
+
+    if fiware_service:
+        headers['Fiware-Service'] = fiware_service
+
+    if fiware_service_path:
+        headers['Fiware-Servicepath'] = fiware_service_path
 
     logger.debug(
         'Going to persist %s (%d) to %s',
@@ -223,7 +240,46 @@ def setup_logger():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Portugal Weather Observed Harvester')
+
+    parser.add_argument('stations', metavar='stations', type=str, nargs='*',
+                        help='Station Codes separated by spaces. ' +
+                        'See https://jmcanterafonseca.carto.com/viz/e7ccc6c6-9e5b-11e5-a595-0ef7f98ade21/public_map')
+    parser.add_argument('--service', metavar='service',
+                        type=str, nargs=1, help='FIWARE Service')
+    parser.add_argument('--service-path', metavar='service_path',
+                        type=str, nargs=1, help='FIWARE Service Path')
+    parser.add_argument('--endpoint', metavar='endpoint',
+                        type=str, nargs=1, help='Context Broker end point. Example. http://orion:1030')
+    parser.add_argument('--latest', action='store_true',
+                        help='Flag to indicate to only harvest the latest observation')
+
+    args = parser.parse_args()
+
+    if args.service:
+        fiware_service = args.service[0]
+
+    if args.service_path:
+        fiware_service_path = args.service_path[0]
+
+    if args.endpoint:
+        orion_service = args.endpoint[0]
+
+    if args.latest:
+        print('Only retrieving latest observations')
+        only_latest = True
+
+    for s in args.stations:
+        stations_to_retrieve_data.append(s)
+
     setup_logger()
+
+    if len(stations_to_retrieve_data) == 0:
+        logger.debug('Retrieving data for all stations ....')
+    else:
+        logger.debug('Only retrieving data for stations: ' +
+                     str(stations_to_retrieve_data))
 
     load_station_data()
 
